af7776f39a663abe563966f5cac4fd9bbdd631bf
vincent_reynard <vincent_reynard@hotmail.com>
2021-12-12 16:38:14 +0700

    fix cookie configuration and client base URL

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,9 @@ const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     // we are on the server
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      // baseURL:
+      //   'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://ticketing.vincentreynard.com',
       headers: req.headers,
     });
   } else {
